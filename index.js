45260d1ca21d2676a6bdc34c564ebbbbc0e9fb52
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-29 23:11:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("div.target");
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  
+  for (var i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var child = document.getElementById('grand-node');
+  var next = child.children[0];
+  while (next) {
+    child = next;
+    next = child.children[0];
+  }
+}
